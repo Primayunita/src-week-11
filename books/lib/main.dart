d072397e7e0f92b2fe4873eed94be8a58d7a527d
Primayunita <primayunitaagustine28@gmail.com>
2026-05-07 09:12:21 +0700

    W11: Soal 3

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -46,6 +46,8 @@ class _FuturePageState extends State<FuturePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
         title: const Text('Back from the Future'),
       ),
       body: Center(
@@ -53,6 +55,24 @@ class _FuturePageState extends State<FuturePage> {
           children: [
             const Spacer(),
 
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {
+              setState(() {});
+
+              getData()
+              .then((value) {
+                result = value.body.toString().substring(0, 450);
+
+                setState(() {});
+            })
+            .catchError((_) {
+              result = 'An error occurred';
+
+              setState(() {});
+            });
+          },
+      ),
             const Spacer(),
             Text(result),
             const Spacer(),
